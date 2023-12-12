a7432bbc92c4e829e3af30e4b47bd0ce8e5e07a9
chartdesign <chartshorne@gmail.com>
2023-12-11 19:08:05 -0800

    login not working

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
+import { AppProvider } from "../components/AppContext";
+import { Toaster } from "react-hot-toast";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Header from "../components/layout/Header";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -13,9 +16,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang='en' className='scroll-smooth'>
       <body className={poppins.className}>
-        <main className='max-w-4xl mx-auto p-4'>{children}</main>
+        <main className='max-w-4xl mx-auto p-4'>
+          <AppProvider>
+            <Toaster />
+            <Header />
+            {children}
+            <footer className='border-t p-8 text-center text-gray-500 mt-16'>
+              &copy; 2023 All rights reserved
+            </footer>
+          </AppProvider>
+        </main>
       </body>
     </html>
   );
